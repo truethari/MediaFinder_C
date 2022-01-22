ca135da4003256d5d80767c5f3fa6d374b1a9aba
Tharindu N <tharindu.nm@yahoo.com>
2022-01-22 09:09:01 +0530

    listFilesRecursively, endsWith

--- media.c
+++ media.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdbool.h>
+
+const char *fileTypes[] = {".mp4", ".mkv", ".avi", ".3gp", ".flv", ".jpg", "jpeg", ".png", ".psd"};
 
 void welcome() {
     int number;
@@ -20,11 +26,41 @@ void welcome() {
     }
 }
 
-void listDir() {
-    
+void listFilesRecursively(char *basePath)
+{
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(basePath);
+
+    // Unable to open directory stream
+    if (!dir)
+        return;
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        {
+            printf("%s/%s\n", basePath, dp->d_name);
+
+            strcpy(path, basePath);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+
+            listFilesRecursively(path);
+        }
+    }
+
+    closedir(dir);
+}
+
+bool endsWith (char* base, char* str) {
+    int blen = strlen(base);
+    int slen = strlen(str);
+    return (blen >= slen) && (0 == strcmp(base + blen - slen, str));
 }
 
 int main() {
-   welcome();
+   //listFilesRecursively("C:/GitHub/MediaFinder_C");
+   //printf("%s\n", endsWith("abc", "c")? "true" : "false");
    return 0;
 }
